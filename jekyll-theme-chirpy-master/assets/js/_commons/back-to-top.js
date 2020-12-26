d249c4348ab1af13c1f6e9dbdb7d5727cdb4a683
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-12-26 10:31:11 -0300

    [Automation] Initialize the environment.

---
+++ jekyll-theme-chirpy-master/assets/js/_commons/back-to-top.js
@@ -0,0 +1,18 @@
+/*
+Reference: https://bootsnipp.com/snippets/featured/link-to-top-page
+*/
+$(function() {
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > 50 &&
+      $("#sidebar-trigger").css("display") === "none") {
+      $("#back-to-top").fadeIn();
+    } else {
+      $("#back-to-top").fadeOut();
+    }
+  });
+
+  $("#back-to-top").click(() => {
+    $("body,html").scrollTop(0);
+    return false;
+  });
+});
